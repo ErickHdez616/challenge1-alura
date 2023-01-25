4076d9c24b82dd5bc4685bdabdd754138aa094ac
Erick Hernández <erickgaming92@gmail.com>
2023-01-25 14:56:03 -0600

    Haciendo varias modificaciones al JS

--- criptador.js
+++ criptador.js
@@ -1,15 +1,40 @@
-var botonesId = document.querySelectorbyId("botonesId");
+var textoInput = document.getElementById("campo1");
+var imagen = document.getElementById("persona");
+var txt1 = document.getElementById("txt1");
+var txt2 = document.getElementById("txt2");
+var salida = document.getElementById("campo2");
+var copiar = document.getElementById("btn3");
 
-function accionA(){
-    botonesId.link();
-    encriptar();
+function reescribirTexto(){
+    valor = textoInput.value;
+    return salida.innerHTML = valor;
 }
 
-function accionB(){
-    botonesId.link();
+function accionBotonEncriptar(){
+    ocultarImg_y_Txt();
+    mostrarCopiar_y_Output();
+    reescribirTexto();
 }
 
-function encriptar(){
-    var textoInput = document.getElementbyId("campo1").value;
-    alert("Haz escrito: " + textoInput);
+function accionBotonDesencriptar(){
+    mostrarImg_y_Txt();
+    ocultarCopiar_y_Output();
+}
+
+function ocultarImg_y_Txt(){ 
+    return [imagen.style.display = "none", txt1.style.display = "none"
+    , txt2.style.display = "none"];
+}
+
+function mostrarImg_y_Txt(){
+    return [imagen.style.display = "block", txt1.style.display = "block"
+    , txt2.style.display = "block"];
+}
+
+function mostrarCopiar_y_Output(){
+    return [salida.style.display = "inline-block", copiar.style.display = "inline-block"];
+}
+
+function ocultarCopiar_y_Output(){
+    return [salida.style.display = "none", copiar.style.display = "none"];
 }
